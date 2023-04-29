121c51da4da01bbb583be0c0e59a41e663f71fda
Stefferson Thallys <sthallys@DESKTOP-PGNO550.localdomain>
2023-04-29 17:21:27 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    globals: {
+        module: 'readonly',
+    },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': ['off'],
+        'react/jsx-uses-react': ['off'],
+        'react/jsx-props-no-spreading': ['warn'],
+        'react/no-unescaped-entities': ['off'],
+    },
+};
