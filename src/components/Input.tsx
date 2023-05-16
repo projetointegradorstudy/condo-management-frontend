25bac79ad10ee162a7f9141b5060fdc44019600d
Natália <nataliaclickz@gmail.com>
2023-05-16 12:28:48 -0300

    Page Register Admin and Label component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,8 @@
 import { InputHTMLAttributes } from 'react';
 import '../styles/input.scss';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  title: string;
-}
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-export function Input({ title, ...props }: InputProps) {
-  return (
-    <>
-      <label>{title}</label>
-      <input {...props} />
-    </>
-  );
+export function Input({ ...props }: InputProps) {
+  return <input {...props} />;
 }
