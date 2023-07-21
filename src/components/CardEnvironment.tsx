c7f69c39fc6e7d00e4c5c36b4f64d04619574ef4
Natália <nataliaclickz@gmail.com>
2023-07-21 14:03:56 -0300

    card environment maintenance disabled

--- src/components/CardEnvironment.tsx
+++ src/components/CardEnvironment.tsx
@@ -1,4 +1,4 @@
-import { Users } from 'phosphor-react';
+import { Barricade, Gear, Users } from 'phosphor-react';
 import { Button } from './Button';
 import { IDataElementProps, IEnvironment } from '../interfaces';
 import imageDefault from '../assets/image-default.png';
@@ -9,36 +9,42 @@ export function CardEnvironment({ data }: IDataElementProps<IEnvironment>) {
   const { setIsReservationModal } = getContext();
 
   return (
-    <>
-      <div className="card-environment">
-        <div className="card-content">
-          <div className="card-image">
-            <img src={data?.image ? data.image : imageDefault} alt="No image" />
-          </div>
-          <div className="card-title">
-            <h3>{data.name}</h3>
-          </div>
-          <div className="card-description">
-            <span>{data.description}</span>
+    <div className={`card-environment ${data.status === 'maintenance' ? 'card-environment-maintenance' : ''}`}>
+      <div
+        style={{ display: data.status === 'maintenance' ? 'flex' : 'none' }}
+        className="card-environment-maintenance-message"
+      >
+        <Gear size={36} weight="fill" className="gear" />
+
+        <span>Em manutenção...</span>
+      </div>
+      <div className="card-content">
+        <div className="card-image">
+          <img src={data?.image ? data.image : imageDefault} alt="No image" />
+        </div>
+        <div className="card-title">
+          <h3>{data.name}</h3>
+        </div>
+        <div className="card-description">
+          <span>{data.description}</span>
+        </div>
+
+        <div className="card-footer">
+          <div className="card-info" title="Quantidade de pessoas">
+            <Users size={20} />
+            <b>{data.capacity}</b>
           </div>
 
-          <div className="card-footer">
-            <div className="card-info" title="Quantidade de pessoas">
-              <Users size={20} />
-              <strong>{data.capacity}</strong>
-            </div>
-
-            <div className="card-button">
-              <Button
-                title="Reservar"
-                onClick={() => {
-                  setIsReservationModal({ isOpen: true, data });
-                }}
-              />
-            </div>
+          <div className="card-button">
+            <Button
+              title="Reservar"
+              onClick={() => {
+                setIsReservationModal({ isOpen: true, data });
+              }}
+            />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
